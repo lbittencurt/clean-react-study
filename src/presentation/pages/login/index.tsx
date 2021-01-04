6b122deee256a34f22d66c9efa1928f56d7cab88
Lucas Bittencurt <bittencurt.lucas@outlook.com>
2021-01-04 14:45:19 -0300

    feat: ensure Login navigates to signup correctly

--- src/presentation/pages/login/index.tsx
+++ src/presentation/pages/login/index.tsx
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+
 import Styles from './login-styles.scss'
 import {
   Footer,
@@ -70,7 +72,11 @@ const Login: React.FC<Props> = ({ validation, authentication }: Props) => {
             disabled={!!state.emailError || !!state.passwordError}
             className={Styles.submit} type="submit"
           >Entrar</button>
-          <span className={Styles.link}>Criar conta</span>
+          <Link
+            data-testid="signup"
+            to="/signup"
+            className={Styles.link}
+          >Criar conta</Link>
           <FormStatus />
         </form>
       </Context.Provider>
