07febda9a3b0d373da692f962bdc680b05a30ceb
Lucas Bittencurt <bittencurt.lucas@outlook.com>
2021-01-12 00:30:12 -0300

    refactor: ensure MinLengthValidation return error if value is invalid

--- src/validation/validators/min-length/min-length-validation.ts
+++ src/validation/validators/min-length/min-length-validation.ts
@@ -2,9 +2,7 @@ import { InvalidFieldError } from '@/validation/errors'
 import { FieldValidation } from '@/validation/protocols'
 
 export class MinLengthValidation implements FieldValidation {
-  fieldName: string
-
-  constructor (readonly field: string, private readonly minLength: number) {}
+  constructor (readonly fieldName: string, private readonly minLength: number) {}
 
   validate (value: string): Error {
     return new InvalidFieldError()
